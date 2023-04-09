8c55b9b359930c40960757da1575ccab34cac589
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-04-10 00:18:46 +0600

    header section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,45 +10,45 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
-      <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to="https://flowbite.com/" class="flex items-center">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+      <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link to="https://flowbite.com/" className="flex items-center">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Job Hunter
             </span>
           </Link>
-          <div class="flex md:order-2">
+          <div className="flex md:order-2">
             <button
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="btn-primary"
             >
               Star Applying
             </button>
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
-              class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded="false"
             >
-              <div className="lg:hidden">
+              <div classNameName="lg:hidden">
                 {/* Dropdown Open Button */}
                 <button
                   aria-label="Open Menu"
                   title="Open Menu"
                   onClick={() => setIsMenuOpen(true)}
                 >
-                  <Bars3BottomRightIcon className="w-5 text-gray-600" />
+                  <Bars3BottomRightIcon classNameName="w-5 text-gray-600" />
                 </button>
                 {isMenuOpen && (
-                  <div className="absolute top-0 left-0 w-full z-10">
-                    <div className="p-5 bg-white border rounded shadow-sm">
+                  <div classNameName="absolute top-0 left-0 w-full z-10">
+                    <div classNameName="p-5 bg-white border rounded shadow-sm">
                       {/* Logo & Button section */}
-                      <div className="flex items-center justify-between mb-4">
+                      <div classNameName="flex items-center justify-between mb-4">
                         <div>
-                          <Link to="/" className="inline-flex items-center">
-                            <BoltIcon className="h-6 w-6 text-blue-500" />
-                            <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                          <Link to="/" classNameName="inline-flex items-center">
+                            <BoltIcon classNameName="h-6 w-6 text-blue-500" />
+                            <span classNameName="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
                               nextPage
                             </span>
                           </Link>
@@ -60,22 +60,22 @@ const Header = () => {
                             title="Close Menu"
                             onClick={() => setIsMenuOpen(false)}
                           >
-                            <XMarkIcon className="w-5 text-gray-600" />
+                            <XMarkIcon classNameName="w-5 text-gray-600" />
                           </button>
                         </div>
                       </div>
                       {/* Mobile Nav Items Section */}
                       <nav>
-                        <ul className="space-y-4">
+                        <ul classNameName="space-y-4">
                           <li>
-                            <Link to="/" className="default">
+                            <Link to="/" classNameName="default">
                               Home
                             </Link>
                           </li>
                           <li>
                             <Link
                               to="/statistics"
-                              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
+                              classNameName="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
                             >
                               Statistics
                             </Link>
@@ -83,7 +83,7 @@ const Header = () => {
                           <li>
                             <Link
                               to="/jobs"
-                              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
+                              classNameName="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
                             >
                               Applied Jobs
                             </Link>
@@ -91,7 +91,7 @@ const Header = () => {
                           <li>
                             <Link
                               to="/blog"
-                              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
+                              classNameName="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
                             >
                               Blog
                             </Link>
@@ -105,14 +105,14 @@ const Header = () => {
             </button>
           </div>
           <div
-            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
                   to="/"
-                  class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                   aria-current="page"
                 >
                   Home
@@ -121,7 +121,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/statistics"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Statistics
                 </Link>
@@ -129,7 +129,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/jobs"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Applied Jobs
                 </Link>
@@ -137,7 +137,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/blog"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Blog
                 </Link>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            home
-        </div>
-    );
+  return (
+    <div className="mt-20 md:flex justify-around items-center my-container bg-gradient-to-r from-[#7E90FE]/0 to-[#9873FF]/100 mb-10">
+      <div className="sm:pt-16">
+        <h1 className="text-5xl font-bold">
+          One Step Closer To Yourbr <br /> <span className="animate-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF]/100 bg-clip-text text-transparent font-black">Dream Job</span>
+        </h1>
+        <p>
+          Explore thousands of job opportunities with all the information you
+          need. Its your future. Come find it. Manage all your job application
+          from start to finish.
+        </p>
+        <Link to="/">
+          <button className="btn-primary">Get Started</button>
+        </Link>
+      </div>
+      <img src="../../public/hardy.png" className="w-[816px] h-[744px] " alt="" srcset="" />
+    </div>
+  );
 };
 
 export default Home;
